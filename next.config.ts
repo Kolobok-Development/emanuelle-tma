e97ca21ad664b349a4fb895f853784a8295c8223
Nikita Savitskiy <s6475515243@gmail.com>
2026-01-10 22:45:41 +0400

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,11 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 's3.twcstorage.ru',
+        pathname: '**',
+      },
     ],
   },
 };
