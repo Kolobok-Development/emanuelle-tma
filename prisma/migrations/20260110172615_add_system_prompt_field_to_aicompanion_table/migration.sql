185117ee07049bdf37a5b4bffbc385b59e545165
Nikita Savitskiy <s6475515243@gmail.com>
2026-02-13 23:04:38 +0400

    fixed migration history

---
+++ prisma/migrations/20260110172615_add_system_prompt_field_to_aicompanion_table/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column isPremium on the AICompanion table. All the data in the column will be lost.
+  - The avatar column on the AICompanion table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - Added the required column publicBio to the AICompanion table without a default value. This is not possible if the table is not empty.
+  - Added the required column systemPrompt to the AICompanion table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."AICompanion" DROP COLUMN "isPremium",
+ADD COLUMN     "publicBio" TEXT NOT NULL,
+ADD COLUMN     "systemPrompt" JSONB NOT NULL,
+DROP COLUMN "avatar",
+ADD COLUMN     "avatar" TEXT[],
+ALTER COLUMN "energyCost" SET DEFAULT 1;
